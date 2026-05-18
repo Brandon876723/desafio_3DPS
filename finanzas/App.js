c358f33bd71de38efb9eb5318fa8df279d2d5237
Centeno Ramos <Centenojesus426@gmail.com>
2026-05-17 20:28:45 -0600

    subiendo App.js

--- finanzas/App.js
+++ finanzas/App.js
@@ -5,8 +5,45 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./src/utils/firebase";
 
 //importamos los screens
+import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
+import HomeScreen from "./src/screens/HomeScreen"
 
-
-
-
+//creamos el navegador tipo stack que manejara las plantillas
 const Stack = createStackNavigator();
+
+export default function App() {
+  //guardamos al usuario actual
+  const [user, setUser] = useState(null);
+
+  //se ejecutara al correr la app
+  useEffect(() => {
+    //sabra si el usuario inicia o cierra sesion
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      //actualizara el estado con el usuario que este
+      setUser(currentUser);
+    });
+    //limpiara el listener cuando desmontemos el componente
+    return unsubscribe;
+  }, []);
+
+  //retornamos al usuario
+  //si el usuario(user) existe nos mostrara el HomeScreen
+  //si no existe nos  mostrara el login y el registro
+  //hara que ssiempre sepa si el usuario esta autenticado
+  //y lo pueda redirigir automaticamente
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {user ? (
+          <Stack.Screen name="Home" component={HomeScreen} />
+        ) : (
+          <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
